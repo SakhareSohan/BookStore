a99b7e0c976c6bb5aace68098f5cd14c74c35b4f
sohan sakhare <sohansakhare2001@gmail.com>
2024-08-17 11:39:52 +0530

    Revert "[sohan] added: user and admin"
    This reverts commit f87b4f2a9479e876639c562d417472ce800b966a.

--- src/interfaces/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  role: {enum: ['admin', 'user']};
-  mobileNo: number;
-  id?: string | number;
-}
